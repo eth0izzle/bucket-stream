f68e18423398e59c272c03dafd8a651515119e72
eth0izzle <eth0@ifc0nfig.com>
2017-11-29 23:59:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Bucket Stream
+
+**Find interesting Amazon S3 Buckets by watching certificate transparency logs.**
+
+This tool simply listens to various certificate transparency logs (via certstream) and attempts to find public S3 buckets from permutations of the certificates domain name.
+
+![Demo](https://i.imgur.com/5S92YXI.png)
+
+**Be responsible**. I mainly created this tool to highlight the risks associated with public S3 buckets and to put a different spin on the usual dictionary-based attacks. Some quick tips if you use S3 buckets:
+
+1) Randomise your bucket names! There is no need to use `company-backup.s3.amazonaws.com`.
+2) Set appropriate permissions and audit regularly. If possible create two buckets - one for your public assets and another for private data.
+3) Be mindful about **your data**. What are suppliers, contractors and third parties doing with it? Where and how is it stored? These basic questions should be addressed in every info sec policy.
+
+Thanks to my good friend David (@riskobscurity) for the idea.
+
+## Installation
+
+Python 3.4+ and pip3 are required. Then just:
+
+1. `git clone https://github.com/eth0izzle/bucket-stream.git`
+2. *(optional)* Create a virtualenv with `pip3 install virtualenv && virtualenv .virtualenv && source .virtualenv/bin/activate`
+2. `pip3 install -r requirements.txt`
+3. `python3 bucket-stream.py`
+
+## Usage
+
+Simply run `python bucket-stream.py`.
+
+If you provide AWS access and secret keys in `config.yaml` Bucket Stream will attempt to identity the buckets owner.
+
+    usage: python bucket-stream.py
+
+    Find interesting Amazon S3 Buckets by watching certificate transparency logs.
+
+    optional arguments:
+      -h, --help           show this help message and exit
+      --only-interesting   Only log 'interesting' buckets whose contents match
+                           anything within keywords.txt (default: False)
+      --skip-lets-encrypt  Skip certs (and thus listed domains) issued by Let's
+                           Encrypt CA (default: False)
+      -t , --threads       Number of threads to spawn. More threads = more power.
+                           (default: 20)
+
+## F.A.Qs
+
+**Nothing appears to be happening**
+Patience! Sometimes certificate transparency logs can be quiet for a few minutes.
+
+**I found something highly confidential**
+**Report it** - please! You can usually figure out the owner from the bucket name or by doing some quick reconnaissance. Failing that contact Amazon's support teams.
+
+## Contributing
+
+1. Fork it, baby!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request.
+
+## License
+
+MIT. See LICENSE
---
+++ bucket-stream.py
@@ -0,0 +1,125 @@
+import argparse, logging
+import yaml
+import boto3
+import certstream
+import tldextract
+import requests
+from requests.adapters import HTTPAdapter
+from queue import Queue
+from threading import Thread
+
+S3_URL = "http://s3-1-w.amazonaws.com"
+BUCKET_HOST = "%s.s3.amazonaws.com"
+QUEUE_SIZE = 100
+CHECKED_BUCKETS = list()
+FOUND_COUNT = 0
+KEYWORDS = [line.strip() for line in open('keywords.txt')]
+BUCKET_QUEUE = Queue(maxsize=QUEUE_SIZE)
+ARGS = argparse.Namespace()
+CONFIG = yaml.safe_load(open('config.yaml'))
+S3_CLIENT = boto3.client('s3', aws_access_key_id=CONFIG['aws_access_key'], aws_secret_access_key=CONFIG['aws_secret'])
+
+
+class BucketWorker(Thread):
+    def __init__(self, q, *args, **kwargs):
+        self.q = q
+        self.session = requests.Session()
+        self.session.mount("http://", HTTPAdapter(pool_connections=ARGS.threads, pool_maxsize=QUEUE_SIZE, max_retries=0))
+
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        global FOUND_COUNT
+
+        while True:
+            try:
+                bucket_url = self.q.get()
+                check_response = self.session.head(S3_URL, timeout=3, headers={"Host": bucket_url})
+
+                if check_response.status_code == 307:  # valid bucket, lets check if its public
+                    new_bucket_url = check_response.headers["Location"]
+                    bucket_response = requests.request('GET' if ARGS.only_interesting else 'HEAD', new_bucket_url, timeout=3)
+
+                    if bucket_response.status_code == 200:  # bucket is public!
+                        if not ARGS.only_interesting or (ARGS.only_interesting and any(keyword in bucket_response.text for keyword in KEYWORDS)):
+                            bucket_owner = None
+
+                            if CONFIG['aws_access_key'] and CONFIG['aws_secret']:
+                                try:
+                                    result = S3_CLIENT.get_bucket_acl(Bucket=bucket_url.replace(".s3.amazonaws.com", ""))
+                                    bucket_owner = result['Owner']['DisplayName']
+                                except:
+                                    pass
+
+                            print("%s is public%s" % (new_bucket_url, (", owned by " + bucket_owner) if bucket_owner is not None else ""))
+                            FOUND_COUNT += 1
+            except:
+                pass
+
+            self.q.task_done()
+
+
+def listen(message, context):
+    if message["message_type"] == "heartbeat":
+        return
+
+    if message["message_type"] == "certificate_update":
+        all_domains = message["data"]["leaf_cert"]["all_domains"]
+
+        if ARGS.skip_lets_encrypt and "Let's Encrypt" in message["data"]["chain"][0]["subject"]["aggregated"]:
+            return
+
+        for domain in set(all_domains):
+            # cut the crap
+            if not domain.startswith("*.") and "cloudflaressl" not in domain and "xn--" not in domain and domain.count("-") < 4 and domain.count(".") < 4:
+                for permutation in get_permutations(tldextract.extract(domain)):
+                    bucket_url = BUCKET_HOST % permutation
+
+                    if bucket_url not in CHECKED_BUCKETS:
+                        CHECKED_BUCKETS.append(bucket_url)
+                        BUCKET_QUEUE.put(bucket_url)
+
+    if len(CHECKED_BUCKETS) % 100 == 0:
+        print("%s buckets checked. %s buckets found" % (len(CHECKED_BUCKETS), FOUND_COUNT))
+
+
+def get_permutations(parsed_domain):
+    perms = [
+        "%s" % parsed_domain.domain,
+        "www-%s" % parsed_domain.domain,
+        "%s-www" % parsed_domain.domain,
+        "%s-%s" % (parsed_domain.subdomain, parsed_domain.domain) if parsed_domain.subdomain else "",
+        "%s-backup" % parsed_domain.domain,
+        "%s-dev" % parsed_domain.domain,
+        "%s-uat" % parsed_domain.domain
+    ]
+
+    return filter(None, perms)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Find interesting Amazon S3 Buckets by watching certificate transparency logs.",
+                                     usage="python bucket-stream.py",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--only-interesting", action="store_true", dest="only_interesting", default=False,
+                        help="Only log 'interesting' buckets whose contents match anything within keywords.txt")
+    parser.add_argument("--skip-lets-encrypt", action="store_true", dest="skip_lets_encrypt", default=False,
+                        help="Skip certs (and thus listed domains) issued by Let's Encrypt CA")
+    parser.add_argument("-t", "--threads", metavar="", type=int, dest="threads", default=20,
+                        help="Number of threads to spawn. More threads = more power.")
+
+    parser.parse_args(namespace=ARGS)
+    logging.disable(logging.WARNING)
+
+    for _ in range(1, ARGS.threads):
+        BucketWorker(BUCKET_QUEUE).start()
+
+    print("Waiting for certstream events - this could take a few minutes to queue up...")
+    certstream.listen_for_events(listen) #blocking
+
+    print("Qutting - waiting for threads to finish up...")
+    BUCKET_QUEUE.join()
+
+
+if __name__ == "__main__":
+    main()
