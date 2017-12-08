dc97f1c318bd1266f1eaff92f8125947f0ab036a
eth0izzle <eth0@ifc0nfig.com>
2017-12-08 12:00:55 +0000

    Retrieve bucket ACLs. Takes in to account rate limiting and uses AWS credentials for checking if provided. General cleanup

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 
 This tool simply listens to various certificate transparency logs (via certstream) and attempts to find public S3 buckets from permutations of the certificates domain name.
 
-![Demo](https://i.imgur.com/B0kesTo.png)
+![Demo](https://i.imgur.com/ZFkIYhD.jpg)
 
 **Be responsible**. I mainly created this tool to highlight the risks associated with public S3 buckets and to put a different spin on the usual dictionary based attacks. Some quick tips if you use S3 buckets:
 
 1) Randomise your bucket names! There is no need to use `company-backup.s3.amazonaws.com`.
 2) Set appropriate permissions and audit regularly. If possible create two buckets - one for your public assets and another for private data.
 3) Be mindful about **your data**. What are suppliers, contractors and third parties doing with it? Where and how is it stored? These basic questions should be addressed in every info sec policy.
-4) Try [Amazon Macie](https://aws.amazon.com/macie/) - it will automatically classify and secure sensitive data.
+4) Try [Amazon Macie](https://aws.amazon.com/macie/) - it can automatically classify and secure sensitive data.
 
 Thanks to my good friend David (@riskobscurity) for the idea.
 
@@ -28,26 +28,32 @@ Python 3.4+ and pip3 are required. Then just:
 
 Simply run `python3 bucket-stream.py`.
 
-If you provide AWS access and secret keys in `config.yaml` Bucket Stream will attempt to access authenticated buckets and identity the buckets owner.
+If you provide AWS access and secret keys in `config.yaml` Bucket Stream will attempt to access authenticated buckets and identity the buckets owner. **Unauthenticated users are severely rate limited.**
 
-    usage: python3 bucket-stream.py
+    usage: python bucket-stream.py
 
     Find interesting Amazon S3 Buckets by watching certificate transparency logs.
 
     optional arguments:
-      -h, --help           show this help message and exit
-      --only-interesting   Only log 'interesting' buckets whose contents match
-                           anything within keywords.txt (default: False)
-      --skip-lets-encrypt  Skip certs (and thus listed domains) issued by Let's
-                           Encrypt CA (default: False)
-      -t , --threads       Number of threads to spawn. More threads = more power.
-                           (default: 20)
+      -h, --help            Show this help message and exit
+      --only-interesting    Only log 'interesting' buckets whose contents match
+                            anything within keywords.txt (default: False)
+      --skip-lets-encrypt   Skip certs (and thus listed domains) issued by Let's
+                            Encrypt CA (default: False)
+      -t , --threads        Number of threads to spawn. More threads = more power.
+                            Limited to 5 threads if unauthenticated.
+                            (default: 20)
+      --ignore-rate-limiting
+                            If you ignore rate limits not all buckets will be
+                            checked (default: False)
+      -l, --log             Log found buckets to a file buckets.log (default:
+                            False)
 
 ## F.A.Qs
 
 - **Nothing appears to be happening**
 
-   Patience! Sometimes certificate transparency logs can be quiet for a few minutes.
+   Patience! Sometimes certificate transparency logs can be quiet for a few minutes. Ideally provide AWS secrets in `config.yaml` is this greatly speeds up the checking speed.
 
 - **I found something highly confidential**
 
--- bucket-stream.py
+++ bucket-stream.py
@@ -1,31 +1,105 @@
-import os, time, argparse, logging
+import os, time, signal, argparse, logging
 import yaml
-import boto3
-import certstream
+from boto3.session import Session
+from certstream.core import CertStreamClient
 import tldextract
 import requests
 from requests.adapters import HTTPAdapter
 from queue import Queue
 from threading import Thread
-from termcolor import colored
+from threading import Lock
+from termcolor import cprint
 
+ARGS = argparse.Namespace()
+CONFIG = yaml.safe_load(open("config.yaml"))
+KEYWORDS = [line.strip() for line in open("keywords.txt")]
 S3_URL = "http://s3-1-w.amazonaws.com"
 BUCKET_HOST = "%s.s3.amazonaws.com"
-QUEUE_SIZE = 100
+QUEUE_SIZE = CONFIG['queue_size']
+UPDATE_INTERVAL = CONFIG['update_interval']  # seconds
+RATE_LIMIT_SLEEP = CONFIG['rate_limit_sleep']  # seconds
+
 CHECKED_BUCKETS = list()
-FOUND_COUNT = 0
-KEYWORDS = [line.strip() for line in open('keywords.txt')]
-BUCKET_QUEUE = Queue(maxsize=QUEUE_SIZE)
-ARGS = argparse.Namespace()
-CONFIG = yaml.safe_load(open('config.yaml'))
-S3_CLIENT = boto3.client('s3', aws_access_key_id=CONFIG['aws_access_key'], aws_secret_access_key=CONFIG['aws_secret'])
+FUND_COUNT = 0
+
+
+class UpdateThread(Thread):
+    def __init__(self, q, *args, **kwargs):
+        self.q = q
+
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        while True:
+            if len(CHECKED_BUCKETS) > 1:
+                cprint("%s buckets checked, %s buckets found" % (len(CHECKED_BUCKETS), FOUND_COUNT), "cyan")
+
+            time.sleep(UPDATE_INTERVAL)
+
+
+class CertStreamThread(Thread):
+    def __init__(self, q, *args, **kwargs):
+        self.q = q
+        self.c = CertStreamClient(self.process, skip_heartbeats=True, on_open=None, on_error=None)
+
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        cprint("Waiting for Certstream events - this could take a few minutes to queue up...", "yellow", attrs=["bold"])
+        self.c.run_forever()
+
+    def process(self, message, context):
+        if message["message_type"] == "heartbeat":
+            return
+
+        if message["message_type"] == "certificate_update":
+            all_domains = message["data"]["leaf_cert"]["all_domains"]
+
+            if ARGS.skip_lets_encrypt and "Let's Encrypt" in message["data"]["chain"][0]["subject"]["aggregated"]:
+                return
+
+            for domain in set(all_domains):
+                # cut the crap
+                if not domain.startswith("*.") and "cloudflaressl" not in domain and "xn--" not in domain and domain.count("-") < 4 and domain.count(".") < 4:
+                    for permutation in get_permutations(tldextract.extract(domain)):
+                        bucket_url = BUCKET_HOST % permutation
+
+                        if bucket_url not in CHECKED_BUCKETS:
+                            CHECKED_BUCKETS.append(bucket_url)
+                            self.q.put(bucket_url)
+
+
+class BucketQueue(Queue):
+    def __init__(self, maxsize):
+        self.lock = Lock()
+        self.rate_limited = False
+        self.next_yield = 0
+
+        super().__init__(maxsize)
+
+    def get(self):
+        with self.lock:
+            t = time.monotonic()
+            if self.rate_limited and t < self.next_yield:
+                time.sleep(self.next_yield - t)
+                t = time.monotonic()
+                self.rate_limited = False
+
+            self.next_yield = t + RATE_LIMIT_SLEEP
+
+        return super().get()
 
 
 class BucketWorker(Thread):
     def __init__(self, q, *args, **kwargs):
         self.q = q
-        self.session = requests.Session()
-        self.session.mount("http://", HTTPAdapter(pool_connections=ARGS.threads, pool_maxsize=QUEUE_SIZE, max_retries=0))
+        self.use_aws = CONFIG["aws_access_key"] and CONFIG["aws_secret"]
+
+        if self.use_aws:
+            self.session = Session(aws_access_key_id=CONFIG["aws_access_key"], aws_secret_access_key=CONFIG["aws_secret"]).resource("s3")
+        else:
+            self.session = requests.Session()
+            self.session.mount("http://", HTTPAdapter(pool_connections=ARGS.threads, pool_maxsize=QUEUE_SIZE, max_retries=0))
 
         super().__init__(*args, **kwargs)
 
@@ -33,73 +107,70 @@ class BucketWorker(Thread):
         while True:
             try:
                 bucket_url = self.q.get()
-                check_response = self.session.head(S3_URL, timeout=3, headers={"Host": bucket_url})
-
-                if check_response.status_code == 307:  # valid bucket, lets check if its public
-                    new_bucket_url = check_response.headers["Location"]
-                    bucket_response = requests.request('GET' if ARGS.only_interesting else 'HEAD', new_bucket_url, timeout=3)
-
-                    if not ARGS.only_interesting or (ARGS.only_interesting and any(keyword in bucket_response.text for keyword in KEYWORDS)):
-                        bucket_name = bucket_url.replace(".s3.amazonaws.com", "")
-
-                        if bucket_response.status_code == 200:
-                            bucket_owner = self.get_bucket_owner(bucket_name)
-                            print(colored("%s is public%s" % (new_bucket_url, ", owned by " + bucket_owner if bucket_owner else ""), 'green', attrs=['bold']))
-                            self.log(new_bucket_url)
-                        elif bucket_response.status_code == 403 and (CONFIG['aws_access_key'] and CONFIG['aws_secret']):
-                            if self.can_access_bucket(bucket_name):
-                                bucket_owner = self.get_bucket_owner(bucket_name)
-                                print(colored("%s is authenticated%s" % (new_bucket_url, ", owned by " + bucket_owner if bucket_owner else ""), 'magenta', attrs=['bold']))
-                                self.log(new_bucket_url)
-            except:
+                self.__check_boto(bucket_url) if self.use_aws else self.__check_http(bucket_url)
+            except Exception as e:
+                print(e)
                 pass
             finally:
                 self.q.task_done()
 
-    def log(self, new_bucket_url):
+    def __check_http(self, bucket_url):
+        check_response = self.session.head(S3_URL, timeout=3, headers={"Host": bucket_url})
+
+        if not ARGS.ignore_rate_limiting and (check_response.status_code == 503 and check_response.reason == "Slow Down"):
+            self.q.rate_limited = True
+            self.q.put(bucket_url)  # add it back to the bucket for re-processing
+        elif check_response.status_code == 307:  # valid bucket, lets check if its public
+            new_bucket_url = check_response.headers["Location"]
+            bucket_response = requests.request("GET" if ARGS.only_interesting else "HEAD", new_bucket_url, timeout=3)
+
+            if bucket_response.status_code == 200 and (not ARGS.only_interesting or (ARGS.only_interesting and any(keyword in bucket_response.text for keyword in KEYWORDS))):
+                cprint("Found bucket '{}'".format(new_bucket_url), "green", attrs=["bold"])
+                self.__log(new_bucket_url)
+
+    def __check_boto(self, bucket_url):
+        bucket_name = bucket_url.replace(".s3.amazonaws.com", "")
+
+        try:
+            self.session.meta.client.head_bucket(Bucket=bucket_name)  # just to check if the bucket exists. Throws NoSuchBucket exception if not
+
+            if not ARGS.only_interesting or (ARGS.only_interesting and self.__bucket_contains_any_keywords(bucket_name)):
+                owner = None
+                acls = None
+
+                try:
+                    acl = self.session.meta.client.get_bucket_acl(Bucket=bucket_name)  # todo: also check IAM policy as it can override ACLs
+                    owner = acl["Owner"]["DisplayName"]
+                    acls = ". ACLs = {} | {}".format(self.__get_bucket_perms(acl, "AllUsers"), self.__get_bucket_perms(acl, "AuthenticatedUsers"))
+                except:
+                    acls = ". ACLS = (could not read)"
+
+                color = "green" if not owner else "magenta"
+                cprint("Found bucket '{}'. Owned by '{}'{}".format(bucket_url, owner if owner else "(unknown)", acls), color, attrs=["bold"])
+                self.__log(bucket_url)
+        except:
+            pass
+
+    def __get_bucket_perms(self, acl, group):
+        group_uri = "http://acs.amazonaws.com/groups/global/%s" % group
+        perms = [g["Permission"] for g in acl["Grants"] if g["Grantee"]["Type"] == "Group" and g["Grantee"]["URI"] == group_uri]
+
+        return "{}: {}".format(group, ", ".join(perms) if perms else "(none)")
+
+    def __bucket_contains_any_keywords(self, bucket_name):
+        try:
+            objects = [o.key for o in self.session.Bucket(bucket_name).objects.all()]
+            return any(keyword in ",".join(objects) for keyword in KEYWORDS)
+        except:
+            return False
+
+    def __log(self, new_bucket_url):
         global FOUND_COUNT
         FOUND_COUNT += 1
 
         if ARGS.log_to_file:
             with open("buckets.log", "a+") as log:
                 log.write("%s%s" % (new_bucket_url, os.linesep))
-
-    def get_bucket_owner(self, bucket_name):
-        try:
-            result = S3_CLIENT.get_bucket_acl(Bucket=bucket_name)
-            return result['Owner']['DisplayName']
-        except:
-            return None
-
-    def can_access_bucket(self, bucket_name):
-        try:
-            S3_CLIENT.list_objects(Bucket=bucket_name)
-            return True
-        except:
-            return False
-
-def listen(message, context):
-    if message["message_type"] == "heartbeat":
-        return
-
-    if message["message_type"] == "certificate_update":
-        all_domains = message["data"]["leaf_cert"]["all_domains"]
-
-        if ARGS.skip_lets_encrypt and "Let's Encrypt" in message["data"]["chain"][0]["subject"]["aggregated"]:
-            return
-
-        for domain in set(all_domains):
-            # cut the crap
-            if not domain.startswith("*.") and "cloudflaressl" not in domain and "xn--" not in domain and domain.count("-") < 4 and domain.count(".") < 4:
-                for permutation in get_permutations(tldextract.extract(domain)):
-                    bucket_url = BUCKET_HOST % permutation
-
-                    if bucket_url not in CHECKED_BUCKETS:
-                        CHECKED_BUCKETS.append(bucket_url)
-                        BUCKET_QUEUE.put(bucket_url)
-
-    if len(CHECKED_BUCKETS) % 100 == 0:
-        print(colored('%s buckets checked. %s buckets found' % (len(CHECKED_BUCKETS), FOUND_COUNT), 'cyan'))
 
 
 def get_permutations(parsed_domain):
@@ -135,24 +206,35 @@ def main():
     parser.add_argument("--skip-lets-encrypt", action="store_true", dest="skip_lets_encrypt", default=False,
                         help="Skip certs (and thus listed domains) issued by Let's Encrypt CA")
     parser.add_argument("-t", "--threads", metavar="", type=int, dest="threads", default=20,
-                        help="Number of threads to spawn. More threads = more power.")
+                        help="Number of threads to spawn. More threads = more power. Limited to 5 threads if unauthenticated.")
+    parser.add_argument("--ignore-rate-limiting", action="store_true", dest="ignore_rate_limiting", default=False,
+                        help="If you ignore rate limits not all buckets will be checked")
     parser.add_argument("-l", "--log", dest="log_to_file", default=False, action="store_true",
                         help="Log found buckets to a file buckets.log")
 
     parser.parse_args(namespace=ARGS)
     logging.disable(logging.WARNING)
 
-    if not CONFIG['aws_access_key'] or not CONFIG['aws_secret']:
-        print(colored('Enter AWS keys in config.yaml to find even more buckets!', 'yellow', 'on_white'))
+    if not CONFIG["aws_access_key"] or not CONFIG["aws_secret"]:
+        cprint("It is highly recommended to enter AWS keys in config.yaml otherwise you will be severely rate limited! You might want to run with --ignore-rate-limiting", "red")
 
-    for _ in range(1, ARGS.threads):
-        BucketWorker(BUCKET_QUEUE).start()
+        if ARGS.threads > 5:
+            cprint("No AWS keys, reducing threads to 5 to help with rate limiting.", "red")
+            ARGS.threads = 5
 
-    print(colored('Waiting for certstream events - this could take a few minutes to queue up...', 'yellow', attrs=['bold']))
-    certstream.listen_for_events(listen) #blocking
+    threads = list()
 
-    print(colored('Qutting - waiting for threads to finish up...', 'yellow', attrs=['bold']))
-    BUCKET_QUEUE.join()
+    try:
+        q = BucketQueue(maxsize=QUEUE_SIZE)
+        threads.extend([BucketWorker(q) for _ in range(0, ARGS.threads)])
+        threads.extend([UpdateThread(q), CertStreamThread(q)])
+        [t.start() for t in threads]
+        BucketWorker(q).start()
+
+        signal.pause()  # pause the main thread
+    except KeyboardInterrupt:
+        cprint("Quitting - waiting for threads to finish up...", "yellow", attrs=["bold"])
+        [t.join() for t in threads]
 
 
 if __name__ == "__main__":
